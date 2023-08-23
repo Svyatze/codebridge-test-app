35a4ba26b7de15dbba8c6b0c36e75c5b21cd8fe4
Svyatoslav Kondratchuk <konrsvyat@gmail.com>
2023-08-23 11:11:39 +0300

    Fixed minor issues.

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ let express = require('express');
 
 let app = express();
 
-app.use(express.static(__dirname+'/dist/angular-cdbrdg-test'));
+app.use(express.static(__dirname+'/dist/space-news-app'));
 
 app.get('/*', (req, res) => {
-  res.sendFile(__dirname+'/dist/angular-cdbrdg-test/index.html')
+  res.sendFile(__dirname+'/dist/space-new-app/index.html')
 });
 
 app.listen(process.env.PORT || 8080)
